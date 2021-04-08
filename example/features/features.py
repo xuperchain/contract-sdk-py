8fb43af7ce762cabcf2903eb0b0e85554733f3d9
chenfengjin <1871653365@qq.coom>
2021-04-08 13:37:15 +0800

    contract-sdk-py mvp

--- example/features/features.py
+++ example/features/features.py
@@ -3,19 +3,20 @@ from xuperchain.driver import Driver
 from xuperchain.contract_method import contract_method
 from xuperchain.response import Response
 
+
 class Features():
 
     @contract_method
-    def initialize(self,ctx:Context):
+    def initialize(self, ctx: Context):
         return Response.ok("ok")
 
     @contract_method
-    def Log(self,ctx:Context):
+    def Log(self, ctx: Context):
         ctx.Log("test log")
         return Response.ok("ok")
 
     @contract_method
-    def QueryTx(self,ctx:Context):
+    def QueryTx(self, ctx: Context):
         txid = ctx.Args().get("txid")
         if txid is None:
             return Response.error("missing txid")
@@ -23,12 +24,13 @@ class Features():
         return Response.ok(resp.blockid)
 
     @contract_method
-    def QueryBlock(self,ctx:Context):
+    def QueryBlock(self, ctx: Context):
         blockid = ctx.Args().get("blockid")
         if blockid is None:
             return Response.error("missing blockid")
         resp = ctx.QueryBlock(blockid=blockid)
-        return Response(resp.blockid)
+        return Response(200, resp.blockid)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Driver().serve(Features())
